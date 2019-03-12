a002b3d189b49959a291b897cdb73014a11d03c6
work8chico <work.chico@gmail.com>
2019-03-12 17:26:39 -0300

    Exceptions e Categorias (feito parcialmente)

--- src/main/java/com/chico/cursomc/domain/Produto.java
+++ src/main/java/com/chico/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 
@@ -28,6 +30,8 @@ public class Produto implements Serializable {
 		joinColumns = @JoinColumn(name = "produto_id"),
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 	)
+	
+	@JsonBackReference
 	private List<Categoria> categorias = new ArrayList<>();
 
 	public Produto() {
