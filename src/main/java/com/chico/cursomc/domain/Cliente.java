7482b02e3679917a1325fc19db1f28aab3c4b252
work8chico <work.chico@gmail.com>
2019-03-21 23:16:45 -0300

    Pedido, EstadoPagamento, Pagamento

--- src/main/java/com/chico/cursomc/domain/Cliente.java
+++ src/main/java/com/chico/cursomc/domain/Cliente.java
@@ -18,7 +18,7 @@ import com.chico.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -37,6 +37,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 	}
@@ -106,6 +109,14 @@ public class Cliente implements Serializable{
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -130,9 +141,5 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
 
 }
---
+++ src/main/java/com/chico/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.chico.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.chico.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
