582f8f1b692eae69f3bb1ca3fd96cbc8e8654f44
work8chico <work.chico@gmail.com>
2019-03-21 22:15:34 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/chico/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.chico.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chico.cursomc.domain.Cliente;
+import com.chico.cursomc.repositories.ClienteRepository;
+import com.chico.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
