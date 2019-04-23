4d4028e52de3badec28172394d1295e5fd5363ee
francisco.a.silva <francisco.a.silva@SS-247671.sistemafiesc.org>
2019-04-23 10:22:18 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/chico/cursomc/services/CategoriaService.java
+++ src/main/java/com/chico/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.chico.cursomc.domain.Categoria;
@@ -43,17 +46,23 @@ public class CategoriaService {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e){
-			
-			throw new DataIntegrityException("Não é possível excliuir uma categoria que possua produtos associados");			
+		} catch (DataIntegrityViolationException e) {
+
+			throw new DataIntegrityException("Não é possível excliuir uma categoria que possua produtos associados");
 		}
 
 	}
 
 	public List<Categoria> findAll() {
-		
+
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+
 	}
 
 }
