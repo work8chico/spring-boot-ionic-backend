efe04424e94596021af716a304ee3792fd88263e
Francisco de Assis <work.chico@gmail.com>
2019-06-18 22:43:54 -0300

    Criação da classe ConfigProd

---
+++ src/main/java/com/chico/cursomc/config/ProdConfig.java
@@ -0,0 +1,37 @@
+package com.chico.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.chico.cursomc.services.DBService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy; 
+	
+	@Bean
+	public Boolean instantiateTest() throws ParseException {
+		
+		if (!"create".equals(strategy)){
+			
+			return false;
+		}
+		
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+	
+	
+
+}
