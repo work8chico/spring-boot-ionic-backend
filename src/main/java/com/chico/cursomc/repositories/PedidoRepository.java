fb48b5584c45363539490b08d2d2330a4f8af3b3
francisco.a.silva <francisco.a.silva@SS-247671.sistemafiesc.org>
2019-10-04 16:57:11 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/chico/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/chico/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,20 @@
 package com.chico.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.chico.cursomc.domain.Cliente;
 import com.chico.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
+
+
--- src/main/java/com/chico/cursomc/services/PedidoService.java
+++ src/main/java/com/chico/cursomc/services/PedidoService.java
@@ -4,16 +4,21 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.chico.cursomc.domain.Cliente;
 import com.chico.cursomc.domain.ItemPedido;
 import com.chico.cursomc.domain.PagamentoComBoleto;
 import com.chico.cursomc.domain.Pedido;
 import com.chico.cursomc.domain.enums.EstadoPagamento;
-import com.chico.cursomc.repositories.ClienteRepository;
 import com.chico.cursomc.repositories.ItemPedidoRepository;
 import com.chico.cursomc.repositories.PagamentoRepository;
 import com.chico.cursomc.repositories.PedidoRepository;
+import com.chico.cursomc.security.UserSS;
+import com.chico.cursomc.services.exceptions.AuthorizationException;
 import com.chico.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,5 +75,19 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();		
+		if(user ==null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		
+		@SuppressWarnings("deprecation")
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+
+	}
+	
 
 }
