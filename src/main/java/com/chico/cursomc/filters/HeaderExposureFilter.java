49f4f7ddbbfadbb130cd3ca06a41b20da47dcd66
FRANCISCO DE ASSIS DA SILVA JUNIOR <francisco.a.silva@fiesc.com.br>
2020-02-26 12:01:27 -0300

    Adicionando arquivos properties

--- src/main/java/com/chico/cursomc/filters/HeaderExposureFilter.java
+++ src/main/java/com/chico/cursomc/filters/HeaderExposureFilter.java
@@ -24,7 +24,7 @@ public class HeaderExposureFilter implements Filter {
 			throws IOException, ServletException {
 		
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("access-control-expose-headers", ",location");
+		res.addHeader("access-control-expose-headers", "location");
 		chain.doFilter(request, response);
 		
 	}
